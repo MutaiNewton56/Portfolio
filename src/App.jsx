e32352c731d447cbaa08aec49779954928548759
Newton <mutainewton56@gmail.com>
2025-03-11 10:44:01 +0300

    updated the use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Head from "./components/Head";
 import Header from "./components/Header";
-
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Facts from "./components/Facts";
@@ -11,28 +14,22 @@ import Resume from "./components/Resume";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
 import PreloaderAndBackToTop from './components/PreloaderAndBackToTop';
 
-// import { useEffect } from 'react';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-
-// useEffect(() => {
-//   AOS.init({
-//     duration: 1000, // Animation duration in ms
-//     once: true, // Whether animation should happen only once
-//   });
-// }, []);
-
-
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in ms
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
+
   return (
     <>
       <Head />
       <Header />
       <Hero />
-      <About/>
+      <About />
       <Facts />
       {/* <Projects /> */}
       <Skills />
@@ -45,4 +42,5 @@ function App() {
     </>
   );
 }
+
 export default App;
