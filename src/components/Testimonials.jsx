aab93c33f6f96c143f8107e53b3458ebbfa10fc9
Newton <mutainewton56@gmail.com>
2025-03-14 14:11:50 +0300

    tailwind

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,7 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import './Testimonials.css';
+
 
 const Testimonials = () => {
     const testimonialsData = [
